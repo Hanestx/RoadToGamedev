049a5ac97a24c6f0217bd87298f8ef04703e7e32
Hanest <53016199+Hanestx@users.noreply.github.com>
2021-10-24 12:06:37 +0300

    Initial commit

---
+++ Assets/CharacterView.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace PlatformerWeb
+{
+    public class CharacterView : MonoBehaviour
+    {
+        public SpriteRenderer SpriteRenderer;
+    }
+}
